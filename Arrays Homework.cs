6bff15dfc2ff4b49ffa568ab688cc9f7026244f6
Unknown <thedovahbrine@gmail.com>
2018-02-22 05:22:45 -0700

    Added Homework and Unity Script

---
+++ Arrays Homework.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrays : MonoBehaviour {
+
+//Unlike functions that use (), Arrays use []
+public GameObject[] title;
+
+//An example of Arrays look like this
+myArray[0] = cat;
+myArray[1] = dog;
+myArray[2] = fish;
+
+/* Arrays work like a list that contains multible variables.
+    It's pesky to write myArray over and over again, so we can write
+    Arrays all in one line like this */
+int[] myArray = {cat, dog, fish};
+
+/* Arrays assign numbers to the values in side them.
+   Arrays do not count starting from One, they start from Zero. 
+   If I was to number the array above it would look like this */
+int[] myArray ={0, 1, 2}
+
+/* Arrays are helpful for finding certain objects in our game. 
+   Think about it you wanted to find all of our Chickens, we could
+   use .FindGameObjectWithTag to find them using an array. */
+
+Public GameObject[] chickens;
+
+void void Start()
+{
+    chickens = GameObject.FindGameObjectWithTag("Chickens");
+}
+
+/*Arrays make use of loops. Loops will iterate through our array. 
+  Say we wanted to count figure out how many chickens
+  we have in our map. We could count them using a loop */
+
+    for(int i = 0; i < chickens.Length; i++)
+    {
+        Debug.Log("There are "+i+" Many chickens" +chickens[i].chickens);
+    }
+}
